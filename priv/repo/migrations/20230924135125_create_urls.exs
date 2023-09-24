5e56a349f3d79ba609224c9a1a9a9ff47a0ee1aa
Jason Stewart <jason.m.stewart@gmail.com>
2023-09-24 09:53:51 -0400

    Add migration for url data model

---
+++ priv/repo/migrations/20230924135125_create_urls.exs
@@ -0,0 +1,14 @@
+defmodule Ogviewer.Repo.Migrations.CreateUrls do
+  use Ecto.Migration
+
+  def change do
+    create table(:urls) do
+      add :url, :text
+      add :status, :string
+      add :preview_image, :text
+      add :error, :text
+
+      timestamps()
+    end
+  end
+end
