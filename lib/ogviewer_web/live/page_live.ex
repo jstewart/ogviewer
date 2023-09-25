b1c78aaa2c32c9c88956c244c9530daf1b390218
Jason Stewart <jason.m.stewart@gmail.com>
2023-09-25 16:44:54 -0400

    Add a spinner

--- lib/ogviewer_web/live/page_live.ex
+++ lib/ogviewer_web/live/page_live.ex
@@ -82,14 +82,22 @@ defmodule OgviewerWeb.PageLive do
     <h1 class="font-bold text-xl mb-3 text-center">OpenGraph Protocol Preview Image Viewer</h1>
     <form class="px-5" phx-submit="preview">
       <div class="flex min-w-full">
-      <div class="w-4/5" >
-      <input class="w-full disabled:bg-gray-200" type="text" autocomplete="off" name="url" value={@url} disabled={@running} />
+        <div class="w-4/5" >
+          <input class="w-full disabled:bg-gray-200" type="text" autocomplete="off" name="url" value={@url} disabled={@running} />
+        </div>
+        <button class="inline-flex ml-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-75" disabled={@running}>
+          <%= if @running do %>
+          <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          <% end %>
+          Preview
+        </button>
+      </div>
       <%= if @error do %>
         <p class="text-red-500"><%= @error %></p>
       <% end %>
-      </div>
-      <button class="ml-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-75" disabled={@running}>Preview</button>
-      </div>
       <div class="w-full px-5 mt-5">
         <%= if @preview_image do %>
           <img src={@preview_image} % />
