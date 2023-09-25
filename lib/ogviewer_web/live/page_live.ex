89ef33743c88e9faf201e228d824a1e8da60a280
Jason Stewart <jason.m.stewart@gmail.com>
2023-09-25 13:56:34 -0400

    Basic styling

--- lib/ogviewer_web/live/page_live.ex
+++ lib/ogviewer_web/live/page_live.ex
@@ -79,14 +79,18 @@ defmodule OgviewerWeb.PageLive do
 
   def render(assigns) do
     ~H"""
-    <h1>OpenGraph Protocol Preview Image Viewer</h1>
-    <form phx-submit="preview">
-      <input type="text" autocomplete="off" name="url" value={@url} disabled={@running} />
+    <h1 class="font-bold text-xl mb-3 text-center">OpenGraph Protocol Preview Image Viewer</h1>
+    <form class="px-5" phx-submit="preview">
+      <div class="flex min-w-full">
+      <div class="w-4/5" >
+      <input class="w-full disabled:bg-gray-200" type="text" autocomplete="off" name="url" value={@url} disabled={@running} />
       <%= if @error do %>
-        <p><%= @error %></p>
+        <p class="text-red-500"><%= @error %></p>
       <% end %>
-      <button disabled={@running}>Preview</button>
-      <div>
+      </div>
+      <button class="ml-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-75" disabled={@running}>Preview</button>
+      </div>
+      <div class="w-full px-5 mt-5">
         <%= if @preview_image do %>
           <img src={@preview_image} % />
         <% end %>
